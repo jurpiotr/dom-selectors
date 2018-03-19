9e07bc95604ce364f815a9af18dc4503909d87db
Piotrek <jurpiotr@gmail.com>
2018-03-19 20:41:09 +0100

    using style, classStyle, dataset

--- src/index.js
+++ src/index.js
@@ -4,3 +4,48 @@ window.highlight = (element) => {
 }
 
 window.h = window.highlight;
+
+window.toggleClass = (element, className) => {
+  if(element.classList.contains(className)){
+    element.classList.remove(className);
+  } else {
+    element.classList.add(className);
+  }
+}
+window.setPlayersStyle = () => {
+  const players = document.querySelectorAll('li[data-type="player"]');
+  const playersArr = Array.from(players);
+  playersArr.forEach((el) => {
+    el.style.background = 'green';
+    el.style.color = 'white';
+  });
+}
+
+window.setAliens = () => {
+  const aliens = document.querySelectorAll('li[data-type="alien"]');
+  const playersArr = Array.from(aliens);
+  playersArr.forEach((el) => {
+    el.style.background = 'orange';
+    el.style.color = 'red';
+  });
+}
+
+window.setAll = () => {
+  window.setAliens();
+  window.setPlayersStyle();
+}
+
+window.setWantedBgColor = (type,background) => {
+  const elements = document.querySelectorAll('li[data-type="' + type + '"]');
+  const elementsArr = Array.from(elements);
+  elementsArr.forEach((el) => {
+    el.style.background = background;
+  });
+}
+
+window.setDifferent = (type) => {
+  window.setWantedBgColor('player','red');
+  window.setWantedBgColor('alien','black');
+  window.setWantedBgColor('predator','orange');
+  window.setWantedBgColor('other','green');
+}
